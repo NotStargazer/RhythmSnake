b58f6a09103d2977a8e20452b8afa13747fc38d0
Rek3dge <samuel.gustafsson@futuregames.nu>
2019-08-29 12:25:46 +0200

    Metronome Art Grid

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    [SerializeField] GameObject gridTile;
+
+    public int width;
+    public int height;
+
+    private void Awake()
+    {
+        DrawGrid();
+    }
+
+    private void DrawGrid()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                var tile = Instantiate(gridTile, transform);
+
+                tile.transform.localPosition = new Vector3(x, y);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Metronome.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Metronome : MonoBehaviour
+{
+    [SerializeField] AudioClip beat;
+    [SerializeField] bool enableMetronome;
+
+    public float bpm;
+
+    int beatNumber;
+    float timeBeforeNextBeat;
+    AudioSource source;
+
+    private void Awake()
+    {
+        source = GetComponent<AudioSource>();
+        bpm = 1 / bpm * 60f;
+    }
+
+    private void Update()
+    {
+        if (!enableMetronome)
+        {
+            return;
+        }
+
+        if (timeBeforeNextBeat <= 0)
+        {
+            timeBeforeNextBeat = bpm;
+            if (beatNumber == 0)
+            {
+                source.pitch *= 2;
+                source.PlayOneShot(beat);
+                source.pitch /= 2;
+            }
+            else
+            {
+                source.PlayOneShot(beat);
+            }
+
+            beatNumber++;
+
+            if (beatNumber > 3)
+            {
+                beatNumber = 0;
+            }
+        }
+
+        timeBeforeNextBeat -= Time.deltaTime;
+    }
+
+    public void MetronomeUpdate()
+    {
+
+    }
+}
